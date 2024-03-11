c7ebaa53bbf47f091cdd3bf335fabb6722bfeffd
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-03-11 10:35:02 +0100

    Neovim default

--- hosts/Laptop/configuration.nix
+++ hosts/Laptop/configuration.nix
@@ -152,7 +152,11 @@
     home-manager
     anytype
   ];
-
+  
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
