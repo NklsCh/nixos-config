98c0ff79c143aa5328b567798939d13650aa545b
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-03-11 10:15:22 +0100

    Alacritty config

--- hosts/Laptop/home.nix
+++ hosts/Laptop/home.nix
@@ -41,7 +41,7 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    ".config".source = ../../modules/dotfiles;
+    ".config/alacritty".source = ../../modules/dotfiles/alacritty;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
