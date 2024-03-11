f4ce3bc29d5f8294400d92098f634ab648c938fa
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-03-11 12:01:11 +0100

    nvchad

--- modules/dotfiles/neovim/nvim.nix
+++ modules/dotfiles/neovim/nvim.nix
@@ -1,19 +1,27 @@
-{ config, pkgs, fetchFromGitHub, ... }:
+{ pkgs, ... }:
 
 let
-  nvchad = pkgs.stdenv.mkDerivation {
-    name = "nvchad";
-    src = pkgs.fetchFromGitHub {
-      owner = "NvChad";
-      repo = "NvChad";
-    };
+  nvchadSource = pkgs.fetchFromGitHub {
+    owner = "NvChad";
+    repo = "NvChad";
+    rev = "c7a4d4e3376b1684c1dd117d53e94480b2d7c9fe";
+    sha256 = "14xskps1h07w2fwj78aw7sd6la0c8w2jwf5cwzsg67ikzr69iqmi";
   };
+
+  installNvChad = ''
+    if [ ! -d "${HOME}/.config/nvim" ]; then
+      cp -r "$nvchadSource"/* "${HOME}/.config/nvim"
+    fi
+  '';
+
 in
   {
     # ... other home manager configurations ...
     programs.neovim = {
       enable = true;
-      package = nvchad;
+      # Use xdg.configFile for a more Nix-idiomatic approach (optional)
+      # xdg.configFile."nvim".source = nvchadSource;
+      after = [ nvchad ];  # Ensure nvchad is built before Neovim
     };
-    xdg.configFile."nvim".source = nvchad;
   }
+
