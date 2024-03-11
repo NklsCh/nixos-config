834284f3704a6e3ff78dce8fd15f34e6783b602e
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-03-11 11:48:16 +0100

    nvchad

--- modules/dotfiles/neovim/nvim.nix
+++ modules/dotfiles/neovim/nvim.nix
@@ -1,13 +1,23 @@
-{ config, pkgs, ... }:
-{
-  progams.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
-    package = with pkgs.vimPlugins; [
-      nvchad
-    ];
+{ pkgs, ... }:
+
+let
+  nvchad = pkgs.stdenv.mkDerivation {
+    name = "nvchad";
+    src = pkgs.fetchFromGitHub {
+      owner = "NvChad";
+      repo = "NvChad";
+      rev = "c7a4d4e3376b1684c1dd117d53e94480b2d7c9fe";
+      sha256 = "14xskps1h07w2fwj78aw7sd6la0c8w2jwf5cwzsg67ikzr69iqmi";
+    };
+    installPhase = ''
+      cp -r $src/* ~/.config/nvim
+      ''
   };
-}
+in
+  {
+    # ... other home manager configurations ...
+    programs.neovim = {
+      enable = true;
+      package = nvchad;
+    };
+  }
