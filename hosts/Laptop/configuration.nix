1e741288953f2dec72fa05237073db8615dabaf1
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-03-13 10:52:31 +0100

    Plugin remove

--- hosts/Laptop/configuration.nix
+++ hosts/Laptop/configuration.nix
@@ -51,7 +51,6 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
-  networking.networkmanager.plugin.vpnc.enable = true;
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
