216fa55426365284ede2d8654b02833c6b5bdc4c
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-04-17 07:42:10 +0200

    Rclone

---
+++ modules/dotfiles/rclone/rclone.nix
@@ -0,0 +1,25 @@
+{ pkgs, pkgs-unstable, ... }:
+{
+  systemd.services.rclone-gdrive = {
+    description = "rclone: Mount gdrive";
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+
+    serviceConfig = {
+      Type = "simple";
+      User = "choinowski";
+      Group = "users";
+
+      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p /home/choinowski/gdrive";
+
+      Environment = "PATH=run/wrappers/bin:/bin:/usr/bin:${pkgs.coreutils}/bin:${pkgs.fuse}/bin";
+      ExecStart = "${pkgs.rclone}/bin/rclone mount gdrive: /home/choinowski/gdrive";
+
+      ExecStop = "/run/wrappers/bin/fusermount3 -u /home/choinowski/gdrive";
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
+  environment.etc."fuse.conf".text = ''
+    user_allow_other
+  '';
+};
