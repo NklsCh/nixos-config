9d23d99a3af0ccd3736ee82951aa94080012f4a0
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-04-17 09:27:15 +0200

    inetutils

--- modules/dotfiles/rclone/rclone.nix
+++ modules/dotfiles/rclone/rclone.nix
@@ -1,7 +1,8 @@
 { pkgs, pkgs-unstable, ... }:
+
 {
   systemd.services.rclone-gdrive = {
-    description = "rclone: Mount gdrive";
+    description = "rclone: Mount Google Drive to /home/choinowski/gdrive";
     after = [ "network-online.target" ];
     wants = [ "network-online.target" ];
 
@@ -9,16 +10,18 @@
       Type = "simple";
       User = "choinowski";
       Group = "users";
-
+      # Ensure the directory exists before trying to mount
       ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p /home/choinowski/gdrive";
-
-      Environment = "PATH=run/wrappers/bin:/bin:/usr/bin:${pkgs.coreutils}/bin:${pkgs.fuse}/bin";
-      ExecStart = "${pkgs.rclone}/bin/rclone mount gdrive: /home/choinowski/gdrive";
-
+      # Use the full path for fusermount3
+      Environment = "PATH=/run/wrappers/bin:/bin:/usr/bin:${pkgs.coreutils}/bin:${pkgs.fuse}/bin";
+      ExecStart = "${pkgs.rclone}/bin/rclone mount gdrive: /home/choinowski/gdrive --config /home/choinowski/.config/rclone/rclone.conf --vfs-cache-mode writes --vfs-cache-max-size 100M --log-level INFO --log-file /tmp/rclone-gdrive.log --umask 022 --allow-other";
+      # Use the full path for fusermount3
       ExecStop = "/run/wrappers/bin/fusermount3 -u /home/choinowski/gdrive";
     };
+
     wantedBy = [ "multi-user.target" ];
   };
+
   environment.etc."fuse.conf".text = ''
     user_allow_other
   '';
