a1f48385d128acfb2cb4d42e7ee69d0e143bf6e4
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-04-22 12:27:24 +0200

    Auto num lock

--- hosts/Laptop/configuration.nix
+++ hosts/Laptop/configuration.nix
@@ -70,7 +70,10 @@
   services.xserver.enable = true;
 
   # Enable the KDE Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm = {
+    enable = true;
+    autoNumlock = true;
+  };
   services.xserver.desktopManager.plasma5.enable = true;
 
 
