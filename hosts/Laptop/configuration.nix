1c19d3610be1a96c69e917d3305c2c4a0c62caeb
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-04-23 14:47:26 +0200

    hyprland

--- hosts/Laptop/configuration.nix
+++ hosts/Laptop/configuration.nix
@@ -74,10 +74,10 @@
   # services.xserver.enable = true;
 
   # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.sddm = {
-  #   enable = true;
-  #   autoNumlock = true;
-  # };
+  services.xserver.displayManager.sddm = {
+    enable = true;
+    autoNumlock = true;
+  };
   # services.xserver.desktopManager.plasma5.enable = true;
 
 
