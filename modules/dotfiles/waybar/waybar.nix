c958c84ab30de0559f416e2458d1a9c344c74380
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-04-24 21:59:23 +0200

    Waybar config?

--- modules/dotfiles/waybar/waybar.nix
+++ modules/dotfiles/waybar/waybar.nix
@@ -8,6 +8,17 @@
       margin-left = 0;
       margin-right = 0;
       spacing = 0;
+      modules-center = [ "hyprland/workspaces" ];
+      modules-right = [ 
+        "pulseaudio"
+        "bluetooth"
+        "battery"
+        "network"
+        "group/hardware"
+        "idle_inhibitor"
+        "tray"
+        "clock"
+      ];
     }];
   };
 }
