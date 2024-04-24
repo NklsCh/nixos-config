a355f846b3262075098d54e26eb9b6ab9f68518e
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-04-24 22:25:20 +0200

    Update waybar.nix

--- modules/dotfiles/waybar/waybar.nix
+++ modules/dotfiles/waybar/waybar.nix
@@ -1,4 +1,5 @@
 { config, pkgs, lib, ... }:
+
 {
   programs.waybar = {
     enable = true;
@@ -9,34 +10,70 @@
         border-bottom: none;
       }
     '';
-
-    # Config
-    
     settings = [{
+      height = 30;
       layer = "top";
-      height = 16;
-      margin-left = 0;
-      margin-right = 0;
-      spacing = 0;
-      modules-center = [ "hyprland/workspaces" ];
-      modules-right = [ 
+      position = "bottom";
+      tray = { spacing = 10; };
+      modules-right = [
         "pulseaudio"
-        "bluetooth"
-        "battery"
         "network"
-        "group/hardware"
-        "idle_inhibitor"
-        "tray"
+        "cpu"
+        "memory"
+        "temperature"
         "clock"
+        "tray"
       ];
-      "hyprland/workspaces" = {
-        on-click = "activate";
-        active-only = false;
-        all-outputs = true;
-        format = "{}";
-        persistent-workspaces = { 
-          "*" = 5;
+      battery = {
+        format = "{capacity}% {icon}";
+        format-alt = "{time} {icon}";
+        format-charging = "{capacity}% ";
+        format-icons = [ "" "" "" "" "" ];
+        format-plugged = "{capacity}% ";
+        states = {
+          critical = 15;
+          warning = 30;
         };
+      };
+      clock = {
+        format-alt = "{:%Y-%m-%d}";
+        tooltip-format = "{:%Y-%m-%d | %H:%M}";
+      };
+      cpu = {
+        format = "{usage}% ";
+        tooltip = false;
+      };
+      memory = { format = "{}% "; };
+      network = {
+        interval = 1;
+        format-alt = "{ifname}: {ipaddr}/{cidr}";
+        format-disconnected = "Disconnected ⚠";
+        format-ethernet = "{ifname}: {ipaddr}/{cidr}   up: {bandwidthUpBits} down: {bandwidthDownBits}";
+        format-linked = "{ifname} (No IP) ";
+        format-wifi = "{essid} ({signalStrength}%) ";
+      };
+      pulseaudio = {
+        format = "{volume}% {icon} {format_source}";
+        format-bluetooth = "{volume}% {icon} {format_source}";
+        format-bluetooth-muted = " {icon} {format_source}";
+        format-icons = {
+          car = "";
+          default = [ "" "" "" ];
+          handsfree = "";
+          headphones = "";
+          headset = "";
+          phone = "";
+          portable = "";
+        };
+        format-muted = " {format_source}";
+        format-source = "{volume}% ";
+        format-source-muted = "";
+        on-click = "pavucontrol";
+      };
+      temperature = {
+        critical-threshold = 80;
+        format = "{temperatureC}°C {icon}";
+        format-icons = [ "" "" "" ];
       };
     }];
   };
