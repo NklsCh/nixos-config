502206767deb5b03e2d4fa1e655977a4ce2b5ea0
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-04-25 08:43:59 +0200

    Typo

--- modules/dotfiles/sddm/sddm.nix
+++ modules/dotfiles/sddm/sddm.nix
@@ -10,7 +10,7 @@ let
 in
   pkgs.stdenv.mkDerivation {
     name = "sddm-theme";
-    src = pkgs.fetchFromGithub {
+    src = pkgs.fetchFromGitHub {
       owner = "MarianArlt";
       repo = "sddm-sugar-dark";
       rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
