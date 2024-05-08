ac1a05df329b4561643af5c2ebe1c7bb8a649fef
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-05-08 08:51:37 +0200

    Changed wlogout import method

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -2,5 +2,6 @@
 {
   imports = [
     ./waybar
+    ./wlogout
   ];
 }
---
+++ modules/hyprland/wlogout/default.nix
@@ -0,0 +1,95 @@
+{ home-manager, ... }:
+{
+  home.file = {
+    ".config/wlogout/layout".text = ''
+      {
+        "label" : "lock",
+        "action" : "swaylock --clock --indicator",
+        "text" : "Lock",
+        "keybind" : "l"
+      }
+      {
+        "label" : "hibernate",
+        "action" : "systemctl hibernate",
+        "text" : "Hibernate",
+        "keybind" : "h"
+      }
+      {
+        "label" : "logout",
+        "action" : "sleep 1; hyprctl dispatch exit",
+        "text" : "Logout",
+        "keybind" : "e"
+      }
+      {
+        "label" : "shutdown",
+        "action" : "systemctl poweroff",
+        "text" : "Shutdown",
+        "keybind" : "s"
+      }
+      {
+        "label" : "suspend",
+        "action" : "systemctl suspend",
+        "text" : "Suspend",
+        "keybind" : "u"
+      }
+      {
+        "label" : "reboot",
+        "action" : "systemctl reboot",
+        "text" : "Reboot",
+        "keybind" : "r"
+      }
+    '';
+    ".config/wlogout/style.css".text = ''
+      * {
+	      background-image: none;
+	      box-shadow: none;
+      }
+
+      window {
+	      background-color: rgba(12, 12, 12, 0.9);
+      }
+
+      button {
+        border-radius: 0;
+        border-color: black;
+	      text-decoration-color: #FFFFFF;
+        color: #FFFFFF;
+	      background-color: #1E1E1E;
+	      border-style: solid;
+	      border-width: 1px;
+	      background-repeat: no-repeat;
+	      background-position: center;
+	      background-size: 25%;
+      }
+
+      button:focus, button:active, button:hover {
+	      background-color: #3700B3;
+	      outline-style: none;
+      }
+
+      #lock {
+        background-image: image(url("./lock.png"), url("./lock.png"));
+      }
+
+      #logout {
+        background-image: image(url("./logout.png"), url("./logout.png"));
+      }
+
+      #suspend {
+        background-image: image(url("./suspend.png"), url("./suspend.png"));
+      }
+
+      #hibernate {
+        background-image: image(url("./hibernate.png"), url("./hibernate.png"));
+      }
+
+      #shutdown {
+        background-image: image(url("./shutdown.png"), url("./shutdown.png"));
+      }
+
+      #reboot {
+        background-image: image(url("./reboot.png"), url("./reboot.png"));
+      }
+    '';
+  };
+}
