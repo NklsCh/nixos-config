380afcb508837d7b15360bfea9c8e1cda8b2dde6
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-05-22 10:21:15 +0200

    p10k

--- modules/core/terminal/p10k/default.nix
+++ modules/core/terminal/p10k/default.nix
@@ -1,6 +1,6 @@
-{ home-manager, pkgs, ... }:
+{ home-manager, config, pkgs, ... }:
 {
   home.file = {
-    ".config/zsh/.p10k.zsh".source = ./p10k-config/p10k.zsh;
+    ".p10k.zsh".source = ./p10k-config/p10k.zsh;
   };
 }
