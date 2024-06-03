5986eda276ad0043965de679971c7b47a18040c6
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-06-03 12:49:35 +0200

    Added Calendar to clock tooltip in waybar

--- modules/hyprland/waybar/laptop.nix
+++ modules/hyprland/waybar/laptop.nix
@@ -6,105 +6,104 @@
       // -*- mode: jsonc -*-
       {
         "spacing": 4,
-            "modules-left": [
-                    "hyprland/workspaces",
-                    "custom/weather",
-                ],
-                "modules-center": [
-                    
-                ],
-                "modules-right": [
-                    "tray",
-                    "pulseaudio",
-                    "network",
-                    "bluetooth",
-                    "battery",
-                    "clock",
-                    "custom/notification"
-                ],
-                "tray": {
-                    // "icon-size": 21,
-                    "spacing": 10
-                },
-                "clock": {
-                    "timezone": "Europe/Berlin",
-                "format": "  {:%H:%M}",
-                    "format-alt": "  {:%d/%m/%Y}"
-                },
-                "battery": {
-                    "states": {
-                        "good": 95,
-                        "warning": 30,
-                        "critical": 15
-                    },
-                    "format": "{icon} {capacity}%",
-                    "format-full": "{icon} {capacity}%",
-                    "format-charging": " {capacity}%",
-                    "format-plugged": " {capacity}%",
-                    "format-alt": "{icon} {time}",
-                    "format-icons": [" ", " ", " ", " ", " "]
-                },
-                "network": {
-                // "format-wifi": "  {essid} ({signalStrength}%)",
-                    "format-wifi": "  {signalStrength}%",
-                "format-ethernet": "  {ipaddr}/{cidr}",
-                    "format-disconnected": "  Disconnected",
-                    "on-click": "alacritty -e sh -c nmtui" 
+        "modules-left": [
+          "hyprland/workspaces",
+          "custom/weather",
+        ],
+        "modules-center": [],
+        "modules-right": [
+          "tray",
+          "pulseaudio",
+          "network",
+          "bluetooth",
+          "battery",
+          "clock",
+          "custom/notification"
+        ],
+        "tray": {
+          // "icon-size": 21,
+          "spacing": 10
+        },
+        "clock": {
+          "timezone": "Europe/Berlin",
+          "format": "  {:%H:%M}",
+          "format-alt": "  {:%d/%m/%Y}",
+          "tooltip-format": "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>"
+        },
+        "battery": {
+          "states": {
+            "good": 95,
+            "warning": 30,
+            "critical": 15
+          },
+          "format": "{icon} {capacity}%",
+          "format-full": "{icon} {capacity}%",
+          "format-charging": " {capacity}%",
+          "format-plugged": " {capacity}%",
+          "format-alt": "{icon} {time}",
+          "format-icons": [" ", " ", " ", " ", " "]
+        },
+        "network": {
+          // "format-wifi": "  {essid} ({signalStrength}%)",
+          "format-wifi": "  {signalStrength}%",
+          "format-ethernet": "  {ipaddr}/{cidr}",
+          "format-disconnected": "  Disconnected",
+          "on-click": "alacritty -e sh -c nmtui" 
+        },
+        "pulseaudio": {
+          "scroll-step": 5, // %, can be a float
+          "format": "{icon} {volume}%",
+          "format-bluetooth": "{volume}% {icon} ",
+          "format-bluetooth-muted": "  Muted",
+          "format-muted": "  Muted",
+          "format-icons": {
+            "headphone": " ",
+            "hands-free": "  ",
+            "headset": "  ",
+            "phone": "  ",
+            "portable": "  ",
+            "car": "  ",
+            "default": [" ", " ", " "]
+          },
+          "on-click": "pavucontrol"
+          },
+          "bluetooth": {
+            // "controller": "controller1", // specify the alias of the controller if there are more than 1 on the system
+            "format": "",
+            "format-disabled": "󰂲",
+            "format-connected": "󰂱",
+            // "tooltip-format": "{controller_alias}\t{controller_address}",
+            // "tooltip-format-connected": "{controller_alias}\t{controller_address}\n\n{device_enumerate}",
+            // "tooltip-format-enumerate-connected": "{device_alias}\t{device_address}",
+            "on-click": "blueman-manager"
+          },
+          "custom/weather": {
+            "format": "{}",
+            "tooltip": false,
+            "interval": 60,
+            "exec": "wttrbar --custom-indicator '{ICON} {temp_C}ºC'",
+            "return-type": "json"
+          },
+          "custom/notification": {
+            "tooltip": false,
+            "format": "{icon}",
+            "format-icons": {
+              "notification": " <span foreground='red'><sup></sup></span>",
+              "none": "",
+              "dnd-notification": " <span foreground='red'><sup></sup></span>",
+              "dnd-none": " ",
+              "inhibited-notification": " <span foreground='red'><sup></sup></span>",
+              "inhibited-none": " ",
+              "dnd-inhibited-notification": " <span foreground='red'><sup></sup></span>",
+              "dnd-inhibited-none": " " 
             },
-                "pulseaudio": {
-                    "scroll-step": 5, // %, can be a float
-                    "format": "{icon} {volume}%",
-                    "format-bluetooth": "{volume}% {icon} ",
-                    "format-bluetooth-muted": "  Muted",
-                    "format-muted": "  Muted",
-                    "format-icons": {
-                        "headphone": " ",
-                        "hands-free": "  ",
-                        "headset": "  ",
-                        "phone": "  ",
-                        "portable": "  ",
-                        "car": "  ",
-                        "default": [" ", " ", " "]
-                    },
-                    "on-click": "pavucontrol"
-                },
-                "bluetooth": {
-                // "controller": "controller1", // specify the alias of the controller if there are more than 1 on the system
-                "format": "",
-                "format-disabled": "󰂲",
-                "format-connected": "󰂱",
-                // "tooltip-format": "{controller_alias}\t{controller_address}",
-                // "tooltip-format-connected": "{controller_alias}\t{controller_address}\n\n{device_enumerate}",
-                // "tooltip-format-enumerate-connected": "{device_alias}\t{device_address}",
-                "on-click": "blueman-manager"
-                },
-            "custom/weather": {
-                "format": "{}",
-                "tooltip": false,
-                "interval": 60,
-                "exec": "wttrbar --custom-indicator '{ICON} {temp_C}ºC'",
-                "return-type": "json"
-                },
-            "custom/notification": {
-                "tooltip": false,
-                "format": "{icon}",
-                "format-icons": {
-                "notification": " <span foreground='red'><sup></sup></span>",
-                "none": "",
-                "dnd-notification": " <span foreground='red'><sup></sup></span>",
-                "dnd-none": " ",
-                "inhibited-notification": " <span foreground='red'><sup></sup></span>",
-                "inhibited-none": " ",
-                "dnd-inhibited-notification": " <span foreground='red'><sup></sup></span>",
-                "dnd-inhibited-none": " " 
-                },
-                "return-type": "json",
-                "exec-if": "which swaync-client",
-                "exec": "swaync-client -swb",
-                "on-click": "swaync-client -t -sw",
-                "on-click-right": "swaync-client -d -sw",
-                "escape": true
-            }
+            "return-type": "json",
+            "exec-if": "which swaync-client",
+            "exec": "swaync-client -swb",
+            "on-click": "swaync-client -t -sw",
+            "on-click-right": "swaync-client -d -sw",
+            "escape": true
+          }
         }
       '';
     ".config/waybar/style.css".text = ''
