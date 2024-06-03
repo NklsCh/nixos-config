af48dfed4ea386b22df7b6447e3d06b46a6cf884
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-06-03 11:57:35 +0200

    Plymouth theme

--- modules/hyprland/waybar/laptop.nix
+++ modules/hyprland/waybar/laptop.nix
@@ -3,234 +3,352 @@
   home-manager.users.${username} = _: {
   home.file = {
     ".config/waybar/config.jsonc".text = ''
+      // -*- mode: jsonc -*-
       {
-        "margin": "5 10 0 10", // top, left, botton, right
-        "layer": "top",
-        "position": "top",
-        "modules-left": ["clock", "hyprland/workspaces", "image#album-art"],
-        "modules-center": [],
-        "modules-right": ["pulseaudio", "backlight", "tray", "battery", "network"],
-
-
-        //***************************
-        //*  Modules configuration  *
-        //***************************
-
-        "clock": {
-          // "timezone": "Europe/Berlin",
-          "tooltip-format": "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>",
-          "format": "{:%d.%m | %H:%M}"
-        },
-
-        "pulseaudio": {
-          // "scroll-step": 1, // %, can be a float
-          "reverse-scrolling": 1,
-          "format": "{volume}% {icon} {format_source}",
-          "format-bluetooth": "{volume}% {icon} {format_source}",
-          "format-bluetooth-muted": " {icon} {format_source}",
-          "format-muted": "{volume}%   {format_source}",
-          "format-source": "",
-          "format-source-muted": "",
-          "format-icons": {
-            "headphone": "",
-            "hands-free": "",
-            "headset": "",
-            "phone": "",
-            "portable": "",
-            "car": "",
-            "default": [" "]
-          },
-          "on-click": "pavucontrol",
-          "min-length": 7,
-        },
-
-        "backlight": {
-          "device": "intel_backlight",
-          "format": "{percent}% {icon}",
-          "format-icons": ["󰃞 ", "󰃟 ", "󰃠 "],
-          "min-length": 7,
-        },
-
-        "battery": {
-          "interval": 3,
-          "states": {
-            "warning": 30,
-            "critical": 15
-          },
-          "format": "{capacity}% {icon}",
-          "format-charging": "{capacity}%  ",
-          "format-plugged": "{capacity}%  ",
-          "format-alt": "{time} {icon}",
-          "format-icons": [" "," "," "," "," "],
-        },
-
-        "tray": {
-          "icon-size": 16,
-          "spacing": 2
-        },
-
-        "network": {
-          "interval": 30,
-          "format-wifi": " {ipaddr}", // Icon: wifi
-          "format-ethernet": "󰈀 ", // Icon: ethernet
-          "format-disconnected": "⚠  Disconnected",
-          "tooltip-format": "{ifname}: {essid}",
-          "on-click": ""
-        },
-    }
-    '';
+        "spacing": 4,
+            "modules-left": [
+                    "hyprland/workspaces",
+                    "custom/weather",
+                ],
+                "modules-center": [
+                    
+                ],
+                "modules-right": [
+                    "tray",
+                    "pulseaudio",
+                    "network",
+                    "bluetooth",
+                    "battery",
+                    "clock",
+                    "custom/notification"
+                ],
+                "tray": {
+                    // "icon-size": 21,
+                    "spacing": 10
+                },
+                "clock": {
+                    "timezone": "Europe/Berlin",
+                "format": "  {:%H:%M}",
+                    "format-alt": "  {:%d/%m/%Y}"
+                },
+                "battery": {
+                    "states": {
+                        "good": 95,
+                        "warning": 30,
+                        "critical": 15
+                    },
+                    "format": "{icon} {capacity}%",
+                    "format-full": "{icon} {capacity}%",
+                    "format-charging": " {capacity}%",
+                    "format-plugged": " {capacity}%",
+                    "format-alt": "{icon} {time}",
+                    "format-icons": [" ", " ", " ", " ", " "]
+                },
+                "network": {
+                // "format-wifi": "  {essid} ({signalStrength}%)",
+                    "format-wifi": "  {signalStrength}%",
+                "format-ethernet": "  {ipaddr}/{cidr}",
+                    "format-disconnected": "  Disconnected",
+                    "on-click": "alacritty -e sh -c nmtui" 
+            },
+                "pulseaudio": {
+                    "scroll-step": 5, // %, can be a float
+                    "format": "{icon} {volume}%",
+                    "format-bluetooth": "{volume}% {icon} ",
+                    "format-bluetooth-muted": "  Muted",
+                    "format-muted": "  Muted",
+                    "format-icons": {
+                        "headphone": " ",
+                        "hands-free": "  ",
+                        "headset": "  ",
+                        "phone": "  ",
+                        "portable": "  ",
+                        "car": "  ",
+                        "default": [" ", " ", " "]
+                    },
+                    "on-click": "pavucontrol"
+                },
+                "bluetooth": {
+                // "controller": "controller1", // specify the alias of the controller if there are more than 1 on the system
+                "format": "",
+                "format-disabled": "󰂲",
+                "format-connected": "󰂱",
+                // "tooltip-format": "{controller_alias}\t{controller_address}",
+                // "tooltip-format-connected": "{controller_alias}\t{controller_address}\n\n{device_enumerate}",
+                // "tooltip-format-enumerate-connected": "{device_alias}\t{device_address}",
+                "on-click": "blueman-manager"
+                },
+            "custom/weather": {
+                "format": "{}",
+                "tooltip": false,
+                "interval": 60,
+                "exec": "wttrbar --custom-indicator '{ICON} {temp_C}ºC'",
+                "return-type": "json"
+                },
+            "custom/notification": {
+                "tooltip": false,
+                "format": "{icon}",
+                "format-icons": {
+                "notification": " <span foreground='red'><sup></sup></span>",
+                "none": "",
+                "dnd-notification": " <span foreground='red'><sup></sup></span>",
+                "dnd-none": " ",
+                "inhibited-notification": " <span foreground='red'><sup></sup></span>",
+                "inhibited-none": " ",
+                "dnd-inhibited-notification": " <span foreground='red'><sup></sup></span>",
+                "dnd-inhibited-none": " " 
+                },
+                "return-type": "json",
+                "exec-if": "which swaync-client",
+                "exec": "swaync-client -swb",
+                "on-click": "swaync-client -t -sw",
+                "on-click-right": "swaync-client -d -sw",
+                "escape": true
+            }
+        }
+      '';
     ".config/waybar/style.css".text = ''
       * {
-        border: none;
-        border-radius: 0;
-        font-family: Liberation Mono;
-        font-size: 13px;
-        min-height: 0;
-      }
+	border: none;
+	border-radius: 10px;
+	font-family: "JetBrainsMono Nerd Font, FontAwesome" ;
+	font-size: 15px;
+	min-height: 10px;
+}
 
-      window#waybar {
-        background: transparent;
-      }
+window#waybar {
+	background: transparent;
+}
 
-      window#waybar.hidden {
-        opacity: 0.2;
-      }
+window#waybar.hidden {
+	opacity: 0.2;
+}
 
-      #workspaces {
-        margin-right: 8px;
-        margin-left: 8px;
-        padding-left: 16px;
-        padding-right: 16px;
-        border-radius: 10px;
-        transition: none;
-        background: #7CAFC2;
-      }
+#window {
+	margin-top: 6px;
+	padding-left: 10px;
+	padding-right: 10px;
+	border-radius: 10px;
+	transition: none;
+    color: transparent;
+	background: transparent;
+}
 
-      #workspaces button {
-        transition: none;
-        color: #ffffff;
-        background: transparent;
-        padding: 5px;
-        font-size: 16px;
-      }
+#workspaces {
+	margin-top: 6px;
+	margin-left: 12px;
+	font-size: 4px;
+	margin-bottom: 0px;
+	border-radius: 10px;
+	background: #ba8baf;
+	transition: none;
+    border-bottom-width: 5px;
+    border-bottom-color: #995b8a;
+    border-bottom-style: solid;
+}
 
-      #workspaces button.urgent {
-        color: #c54626; 
-      }
+#workspaces button {
+	transition: none;
+	color: #ffffff;
+	background: transparent;
+	font-size: 16px;
+	border-radius: 2px;
+}
 
-      /* https://github.com/Alexays/Waybar/wiki/FAQ#the-workspace-buttons-have-a-strange-hover-effect */
-      #workspaces button:hover {
-        transition: none;
-        box-shadow: inherit;
-        text-shadow: inherit;
-        border-radius: inherit;
+#workspaces button.occupied {
+	transition: none;
+	color: #000000;
+	background: transparent;
+	font-size: 4px;
+}
+
+#workspaces button.active {
+	color: #000000;
+}
+
+#workspaces button:hover {
+	transition: none;
+	box-shadow: inherit;
+	text-shadow: inherit;
+	border-color: #ba8baf;
+	color: #ffffff;
+}
+
+#workspaces button.active:hover {
+    color: #000000;
+}
+
+#network {
+	margin-top: 6px;
+	margin-left: 8px;
+	padding-left: 8px;
+	padding-right: 10px;
+	margin-bottom: 0px;
+	border-radius: 10px;
+	transition: none;
+	color: #000000;
+	background: #ba8baf;
+    border-bottom-width: 5px;
+    border-bottom-color: #995b8a;
+    border-bottom-style: solid;
+}
+
+#pulseaudio {
+	margin-top: 6px;
+	margin-left: 8px;
+	padding-left: 8px;
+	padding-right: 10px;
+	margin-bottom: 0px;
+	border-radius: 10px;
+	transition: none;
+	color: #000000;
+	background: #ba8baf;
+    border-bottom-width: 5px;
+    border-bottom-color: #995b8a;
+    border-bottom-style: solid;
+}
+
+#battery {
+	margin-top: 6px;
+	margin-left: 8px;
+	padding-left: 8px;
+	padding-right: 10px;
+	margin-bottom: 0px;
+	border-radius: 10px;
+	transition: none;
+	color: #000000;
+	background: #ba8baf;
+    border-bottom-width: 5px;
+    border-bottom-color: #995b8a;
+    border-bottom-style: solid;
+}
+
+#battery.charging, #battery.plugged {
+	color: #000000;
+    background-color: #ba8baf;
+    border-bottom-width: 5px;
+    border-bottom-color: #995b8a;
+    border-bottom-style: solid;
+}
+
+#battery.critical:not(.charging) {
+    background-color: #ba8baf;
+    color: #000000;
+    animation-name: blink;
+    animation-duration: 0.5s;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+    border-bottom-width: 5px;
+    border-bottom-color: #995b8a;
+    border-bottom-style: solid;
+}
+
+@keyframes blink {
+    to {
+        background-color: #ba8baf;
         color: #000000;
-        background: #7CAFC2;
-      }
+    }
+}
 
-      #workspaces button.active {
-        color: #000000;
-      }
+#clock {
+	margin-top: 6px;
+	margin-left: 8px;
+	padding-left: 8px;
+	padding-right: 10px;
+	margin-bottom: 0px;
+	border-radius: 10px;
+	transition: none;
+	color: #000000;
+	background: #ba8baf;
+	border-bottom-width: 5px;
+    border-bottom-color: #995b8a;
+    border-bottom-style: solid;
+}
 
-      #clock {
-        padding-left: 16px;
-        padding-right: 16px;
-        border-radius: 10px;
-        transition: none;
-        color: #000000;
-        background: #BA8BAF;
-      }
+#tray {
+	margin-top: 6px;
+	margin-left: 8px;
+	padding-left: 8px;
+	margin-bottom: 0px;
+	padding-right: 10px;
+	border-radius: 10px;
+	transition: none;
+	color: #000000;
+	background: #292929;
+    border-bottom-width: 5px;
+    border-bottom-color: #3c3c3c;
+    border-bottom-style: solid;
+}
 
-      #pulseaudio {
-        margin-right: 8px;
-        padding-left: 16px;
-        padding-right: 16px;
-        border-radius: 10px;
-        transition: none;
-        color: #000000;
-        background: #F7B56C;
-      }
+#custom-notification {
+    margin-top: 6px;
+    margin-left: 8px;
+	margin-right: 8px;
+    padding-left: 10px;
+    margin-bottom: 0px;
+    padding-right: 15px;
+    border-radius: 10px;
+    transition: none;
+    color: #000000;
+    background: #ba8baf;
+    border-bottom-width: 5px;
+    border-bottom-color: #995b8a;
+    border-bottom-style: solid;
+}
 
-      #pulseaudio.muted {
-        background-color: #AB4642;
-        color: #000000;
-      }
+#custom-weather {
+	margin-top: 6px;
+	margin-left: 8px;
+	margin-right: 8px;
+	padding-left: 10px;
+	margin-bottom: 0px;
+	padding-right: 15px;
+	border-radius: 10px;
+	transition: none;
+	color: #000000;
+	background: #ba8baf;
+	border-bottom-width: 5px;
+	border-bottom-color: #995b8a;
+	border-bottom-style: solid;
+}
 
-      #backlight {
-        margin-right: 8px;
-        padding-left: 16px;
-        padding-right: 16px;
-        border-radius: 10px;
-        transition: none;
-        color: #000000;
-        background: #A1B56C;
-      }
+#bluetooth {
+    margin-top: 6px;
+    margin-left: 8px;
+    padding-left: 8px;
+    margin-bottom: 0px;
+    padding-right: 10px;
+    border-radius: 10px;
+    transition: none;
+    color: #000000;
+    background: #ba8baf;
+    border-bottom-width: 5px;
+    border-bottom-color: #995b8a;
+    border-bottom-style: solid;
+}
 
-      #battery {
-        margin-right: 8px;
-        padding-left: 16px;
-        padding-right: 16px;
-        border-radius: 10px;
-        transition: none;
-        color: #000000;
-        background: #8FBCBB;
-      }
+#window {
+    margin-top: 6px;
+    margin-left: 8px;
+    padding-left: 8px;
+    margin-bottom: 0px;
+    padding-right: 10px;
+    border-radius: 10px;
+    transition: none;
+    color: #000000;
+    background: #ba8baf;
+    border-bottom-width: 5px;
+    border-bottom-color: #995b8a;
+    border-bottom-style: solid;
+}
 
-      #battery.charging {
-        color: #000000;
-        background-color: #8FBCBB;
-      }
-
-      #battery.warning:not(.charging) {
-        background-color: #F7CA88;
-        color: black;
-      }
-
-      #battery.critical:not(.charging) {
-        background-color: #BF616A;
-        color: #000000;
-        animation-name: blink;
-        animation-duration: 0.5s;
-        animation-timing-function: linear;
-        animation-iteration-count: infinite;
-        animation-direction: alternate;
-      }
-
-      #tray {
-        margin-right: 8px;
-        padding-left: 16px;
-        padding-right: 16px;
-        border-radius: 10px;
-        transition: none;
-        color: #F8F8F8;
-        background: #000000;
-      }
-
-      #network {
-        padding-left: 16px;
-        padding-right: 16px;
-        border-radius: 10px;
-        transition: none;
-        color: #000000;
-        background: #BA8BAF;
-      }
-
-      #network.format-disconncted {
-        background-color: #F7CA88;
-      }
-
-      #album-art {
-        padding-left: 16px;
-        padding-right: 16px;
-        border-radius: 10px;
-      }
-
-      @keyframes blink {
-        to {
-          background-color: #ffffff;
-          color: #000000;
-      }
-      }
+window#waybar.empty #window {  
+ background:none;  
+ border-bottom-width: 5px;
+ border-bottom-color: transparent;
+ border-bottom-style: solid;
+}  
     '';
   };
   };
