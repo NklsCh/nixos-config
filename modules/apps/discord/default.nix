57ee1dcfacb4728b1b85688cb635c2a57d5246ae
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-06-17 09:04:38 +0200

    chore: removed unnecessry discord packages

--- modules/apps/discord/default.nix
+++ modules/apps/discord/default.nix
@@ -17,10 +17,6 @@
   };
 
   environment.systemPackages = with pkgs; [
-    (writeShellScriptBin "discord" ''
-      exec ${(discord.override {withVencord=true;})}/bin/discord --enable-features=UseOzonePlatform --ozone-platform=wayland
-    '')
-    # (discord.override { withVencord = true; })
     (writeShellScriptBin "vesktop" ''
       exec ${vesktop}/bin/vesktop --enable-features=UseOzonePlatform --ozone-platform=wayland
     '')
