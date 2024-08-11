dcc0d87b1aa96e6d9e6e24a82da89dbfd7f4c792
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-08-11 18:45:07 +0200

    chore: Sound has no longer any effect & updated package repos

--- modules/hardware/audio/default.nix
+++ modules/hardware/audio/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
-  sound.enable = true;
+  # sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   environment.systemPackages = with pkgs; [ pulseaudio ];
