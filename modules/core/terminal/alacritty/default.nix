9a91cc59a0712538ce32e7bd1cd4d714dd24ee7a
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-09-02 10:25:51 +0200

    Feat: Stylix

--- modules/core/terminal/alacritty/default.nix
+++ modules/core/terminal/alacritty/default.nix
@@ -1,80 +1,10 @@
-{ username, ... }:
+{ pkgs, username, ... }:
 {
+  environment.systemPackages = with pkgs; [ alacritty ];
+
   home-manager.users.${username} = {
-  programs.alacritty = {
-    enable = true;
-    settings = {
-      window = {
-        opacity = 0.90;
-      };
-      font = {
-        size = 12;
-          normal.family = "JetBrainsMono Nerd Font";
-      };
-      colors = {
-        primary = {
-          background = "#282a36";
-          foreground = "#f8f8f2";
-          bright_foreground = "#ffffff";
-        };
-        cursor = {
-          text = "#282a36";
-          cursor = "#f8f8f2";
-        };
-        vi_mode_cursor = {
-          text = "CellBackground";
-          cursor = "CellForeground";
-        };
-        selection = {
-          text = "CellForeground";
-          background = "#44475a";
-        };
-        normal = {
-          black = "#21222c";
-          red = "#ff5555";
-          green = "#50fa7b";
-          yellow = "#f1fa8c";
-          blue = "#bd93f9";
-          magenta = "#ff79c6";
-          cyan = "#8be9fd";
-          white = "#f8f8f2";
-        };
-        bright = {
-          black = "#6272a4";
-          red = "#ff6e6e";
-          green = "#69ff94";
-          yellow = "#ffffa5";
-          blue = "#d6acff";
-          magenta = "#ff92df";
-          cyan = "#a4ffff";
-          white = "#ffffff";
-        };
-        search = {
-          matches = {
-            foreground = "#44475a";
-            background = "#50fa7b";
-          };
-          focused_match = {
-            foreground = "#44475a";
-            background = "#ffb86c";
-          };
-        };
-        footer_bar = {
-          background = "#282a36";
-          foreground = "#f8f8f2";
-        };
-        hints = {
-          start = {
-            foreground = "#282a36";
-            background = "#f1fa8c";
-          };
-          end = {
-            foreground = "#f1fa8c";
-            background = "#282a36";
-          };
-        };
-      };
+    home.file = {
+      ".config/alacritty/alacritty.toml".source = ./alacritty.toml;
     };
-  };
   };
 }
