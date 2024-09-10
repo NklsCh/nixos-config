d68570acf04e7b298142653a51f82cd09b694b82
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-09-10 14:55:51 +0200

    chore: Fixed default file opener

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -82,18 +82,25 @@
     '';
   };
 
-  xdg.portal = {
-    enable = true;
-    config = {
-      common = {
-        default = [
-          "xdph"
-          "gtk"
-        ];
-        "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
-        "org.freedesktop.portal.FileChooser" = [ "xdg-desktop-portal-gtk" ];
+  xdg = {
+    mime = {
+      defaultApplications = {
+        "inode/directory" = [ "nemo.desktop" ];
       };
     };
-    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+    portal = {
+      enable = true;
+      config = {
+        common = {
+          default = [
+            "xdph"
+            "gtk"
+          ];
+          "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+          "org.freedesktop.portal.FileChooser" = [ "xdg-desktop-portal-gtk" ];
+        };
+      };
+      extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+    };
   };
 }
