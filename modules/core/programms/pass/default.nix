ddd04be446e661a4f60eb209e08950dd394394af
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-10-10 09:06:37 +0200

    feat: Browserpass app

--- modules/core/programms/pass/default.nix
+++ modules/core/programms/pass/default.nix
@@ -1,17 +1,28 @@
 { username, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [ 
-    pass-wayland
+    pass
     gnupg
     pinentry-curses
     passExtensions.pass-otp
     qtpass
+    vivaldi
   ];
+
+  environment.variables = {
+    PASSWORD_STORE_ENABLE_EXTENSIONS = "true";
+  };
+
   home-manager.users.${username} = _: {
     home.file = {
       ".gnupg/gpg-agent.conf".text = ''
-        pinentry-program /run/current-system/sw/bin/pinentry-curses
+        use-agent
+        pinentry-mode loopback
+        pinentry-program /run/current-system/sw/bin/pinentry-tty
+        allow-loopback-pinentry
       '';
     };
   };
+
+  programs.browserpass.enable = true;
 }
