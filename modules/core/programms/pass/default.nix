7c8d6ff9fca63851ee250491052bf4a486b8a5fc
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-10-10 09:13:15 +0200

    fix: error with gpg agent

--- modules/core/programms/pass/default.nix
+++ modules/core/programms/pass/default.nix
@@ -16,8 +16,6 @@
   home-manager.users.${username} = _: {
     home.file = {
       ".gnupg/gpg-agent.conf".text = ''
-        use-agent
-        pinentry-mode loopback
         pinentry-program /run/current-system/sw/bin/pinentry-tty
         allow-loopback-pinentry
       '';
