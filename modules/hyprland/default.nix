8ae409933f4d5b712fc66bccbaff64da6d7a36f1
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-10-14 11:05:17 +0200

    feat: added the ability for hyprlock to unlock gnome-keyring

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -75,7 +75,12 @@
   };
 
   security.pam.services.login.enableGnomeKeyring = true;
-  security.pam.services.hyprlock = {};
+  security.pam.services.hyprlock = {
+    text = ''
+      auth      required  pam_gnome_keyring.so
+      session   required  pam_gnome_keyring.so auto_start
+    '';
+  };
 
   xdg = {
     mime = {
