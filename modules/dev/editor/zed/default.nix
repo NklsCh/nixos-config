7db38cdd4a61e364f8fe463ec304ec2fcd61581a
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-10-23 23:55:56 +0200

    chore: Removed redundant zed settings file

--- modules/dev/editor/zed/default.nix
+++ modules/dev/editor/zed/default.nix
@@ -1,27 +1,9 @@
 {
   pkgs,
-  username,
   ...
 }:
 {
   environment.systemPackages = with pkgs; [
     zed-editor
   ];
-  home-manager.users.${username} = {
-    home.file = {
-      ".config/zed/settings.json".text = ''
-        {
-          "telemetry": {
-            "metrics": false
-          },
-          "theme": "Dracula",
-          "vim_mode": true,
-          "ui_font_size": 15,
-          "buffer_font_size": 13,
-          "buffer_font_family": "JetBrainsMono Nerd Font",
-          "format_on_save": "on"
-        }
-      '';
-    };
-  };
 }
