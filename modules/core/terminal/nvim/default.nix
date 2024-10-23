c6d43093b6ab00d4da4d50c024c98da9fbb32978
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-10-23 11:58:37 +0200

    feat: Added neovim conf | nvchad

--- modules/core/terminal/nvim/default.nix
+++ modules/core/terminal/nvim/default.nix
@@ -1,9 +1,15 @@
-{ pkgs, ... }:
+{ pkgs, home-manager, username, ... }:
 {
   programs.neovim = {
     enable = true;
     vimAlias = true;
     viAlias = true;
     defaultEditor = true;
+  };
+
+  home-manager.users.${username} = _: {
+    home.file = {
+      ".config/nvim".source = ./nvchad;
+    };
   };
 }
