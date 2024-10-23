9b168004e67de554ba32fe14666bf32e18d433ea
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-10-23 21:26:35 +0200

    feat: Added discord presence for neovim

--- modules/core/terminal/nvim/nvchad/lua/plugins/init.lua
+++ modules/core/terminal/nvim/nvchad/lua/plugins/init.lua
@@ -22,4 +22,11 @@ return {
       },
     },
   },
+
+  {
+    'vyfor/cord.nvim',
+    build = './build || .\\build',
+    event = 'VeryLazy',
+    opts = {}, -- calls require('cord').setup()
+  },
 }
