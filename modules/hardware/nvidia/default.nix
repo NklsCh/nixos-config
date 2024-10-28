340cd201a79fd9b3071d76f315b45d72a950cd43
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-10-28 11:45:08 +0100

    feat: Updated packages

--- modules/hardware/nvidia/default.nix
+++ modules/hardware/nvidia/default.nix
@@ -11,6 +11,6 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
   };
 }
