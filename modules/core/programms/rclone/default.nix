b66b6179ffe63602fa4eec376c1b0cc865c5b8a5
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-10-28 11:55:53 +0100

    chore: Reworked rclone mounting point for gdrive

--- modules/core/programms/rclone/default.nix
+++ modules/core/programms/rclone/default.nix
@@ -4,7 +4,7 @@
   environment.systemPackages = with pkgs; [ rclone ];
 
   systemd.services.rclone-gdrive = {
-    description = "rclone: Mount Google Drive to ~/gdrive";
+    description = "rclone: Mount Google Drive to ~/Google Drive";
     after = [ "network-online.target" ];
     wants = [ "network-online.target" ];
 
@@ -13,12 +13,12 @@
       User = "${username}";
       Group = "users";
       # Ensure the directory exists before trying to mount
-      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p /home/${username}/gdrive";
+      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p /home/${username}/\"Google Drive\"";
       # Use the full path for fusermount3
       Environment = "PATH=/run/wrappers/bin:/bin:/usr/bin:${pkgs.coreutils}/bin:${pkgs.fuse}/bin";
-      ExecStart = "${pkgs.rclone}/bin/rclone mount gdrive: /home/${username}/gdrive --config /home/${username}/.config/rclone/rclone.conf --vfs-cache-mode writes --vfs-cache-max-size 100M --log-level INFO --log-file /tmp/rclone-gdrive.log";
+      ExecStart = "${pkgs.rclone}/bin/rclone mount gdrive: /home/${username}/\"Google Drive\" --config /home/${username}/.config/rclone/rclone.conf --vfs-cache-mode writes --vfs-cache-max-size 100M --log-level INFO --log-file /tmp/rclone-gdrive.log";
       # Use the full path for fusermount3
-      ExecStop = "/run/wrappers/bin/fusermount3 -u /home/${username}/gdrive";
+      ExecStop = "/run/wrappers/bin/fusermount3 -u /home/${username}/\"Google Drive\"";
     };
 
     wantedBy = [ "multi-user.target" ];
