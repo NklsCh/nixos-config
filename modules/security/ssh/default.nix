ca0bb2d6316128024f477c24acf548b328d61801
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-10-29 20:12:18 +0100

    feat: New ssh key

--- modules/security/ssh/default.nix
+++ modules/security/ssh/default.nix
@@ -2,7 +2,7 @@
 {
   users.users.${username} = {
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKZrLPuqA5vUxuNNn/yLrDThwgSFdpjNRKMGxEX9dRCN ${username}"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKRcnvF2EYThdhGp45gfbOck/ZXDZR5CrBiMm0eIx3Vl choinowski@Desktop"
     ];
   };
 
