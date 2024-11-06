d920d8228d66a3aaf6f08e749518a1f2c8f4c8cd
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-11-06 14:37:35 +0100

    chore: Added ssh support to gnome keyring

--- modules/core/services/gnome.nix
+++ modules/core/services/gnome.nix
@@ -1,6 +1,13 @@
-{ ... }:
+{ username, ... }:
 {
-  services.gnome = {
-    gnome-keyring.enable = true;
+  home-manager.users.${username} = {
+    services.gnome-keyring = {
+      enable = true;
+      components = [
+        # "pkcs11"
+        "secrets"
+        "ssh"
+      ];
+    };
   };
 }
