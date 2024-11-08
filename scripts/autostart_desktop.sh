cea7142b245cb67f62ba3db413bd02a2bf1c1868
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-11-08 12:55:12 +0100

    refactor: update Nix configurations and improve autostart script for better workspace management

--- scripts/autostart_desktop.sh
+++ scripts/autostart_desktop.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
-gnome-keyring-deamon --start --components=secrets &
 
+# Start background services
+gnome-keyring-daemon --start --components=secrets &
 hyprpaper &
 nm-applet --indicator &
 ags &
 hyprpanel &
 dunst &
 
+# Start applications
+signal-desktop --start-in-tray &
+localsend &
 vesktop &
-sleep 5
-hyprctl dispatch movetoworkspace 4 $(hyprctl clients | grep vesktop | awk '{print $2}')
-
-spotify &
-sleep 5
-hyprctl dispatch movetoworkspace 4 $(hyprctl clients | grep spotify | awk '{print $2}')
-
-
 steam &
+
 sleep 7
-hyprctl dispatch movetoworkspace 3 $(hyprctl clients | grep steam | awk '{print $2}')
+spotify &
+
+# Wait for Steam to fully start
+sleep 5
+for window in $(hyprctl clients | grep -i "steam" | awk '{print $2}'); do
+    hyprctl dispatch movetoworkspace 3,$window
+done
