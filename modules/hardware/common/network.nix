61b891001a4b92bdcc586ddb5a57a72fec102f3d
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-11-08 12:00:53 +0100

    refactor: add firewall configuration for localsend

--- modules/hardware/common/network.nix
+++ modules/hardware/common/network.nix
@@ -6,7 +6,12 @@
       wifi.powersave = true;
     };
     inherit hostName;
+    firewall = {
+      allowedTCPPorts = [ 53317 ];
+      allowedUDPPorts = [ 53317 ];
+    };
   };
+
   users.users.${username} = {
     extraGroups = [ "networkmanager" ];
   };
