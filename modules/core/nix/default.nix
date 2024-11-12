1666b26f9b230052e179cc33042b94f8aab5c96d
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-11-12 14:41:33 +0100

    chore: Added to account to nix store trusted for devenv

--- modules/core/nix/default.nix
+++ modules/core/nix/default.nix
@@ -19,6 +19,11 @@
       "nix-command"
       "flakes"
     ];
+    settings = {
+      trusted-users = [ "root" "choinowski" ];  # Add your username here
+      extra-substituters = [ "https://devenv.cachix.org" ];
+      extra-trusted-public-keys = [ "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=" ];
+    };
   };
 
   home-manager.users.${username} = {
