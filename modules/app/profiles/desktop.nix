c056464807788fdc62370be42b55219a7c8f3445
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-11-14 10:14:28 +0100

    feat: add Teams for Linux configuration and JSON settings

--- modules/app/profiles/desktop.nix
+++ modules/app/profiles/desktop.nix
@@ -1,6 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, username, ... }:
 {
   environment.systemPackages = with pkgs; [
     teams-for-linux
   ];
+
+  home-manager.users.${username} = {
+    home.file.".config/teams-for-linux/config.json".source = ../../config/t4l.json;
+  };
 }
