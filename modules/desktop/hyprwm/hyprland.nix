0fd715216db380389e7eb879d8cb852927a6eaed
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-11-20 09:57:53 +0100

    feat: reorganize Nix configuration by replacing options.nix with settings.nix and updating settings structure

--- modules/desktop/hyprwm/hyprland.nix
+++ modules/desktop/hyprwm/hyprland.nix
@@ -55,13 +55,12 @@ in
             size = 3
             passes = 1
           }
-        }
-
-        shadow {
-          enabled = true
-          range = 4
-          render_power = 3
-          color = rgba(1a1a1aee)
+          shadow {
+            enabled = true
+            range = 4
+            render_power = 3
+            color = rgba(1a1a1aee)
+          }
         }
 
         animations {
