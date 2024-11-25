709e877ef7f15d5e08aa3d93fe04b7615073f771
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-11-25 19:12:31 +0100

    feat: add element-desktop-wayland and session-desktop to system packages

--- modules/app/common/communication.nix
+++ modules/app/common/communication.nix
@@ -1,7 +1,9 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
+    element-desktop-wayland
     localsend
+    session-desktop
     signal-desktop
     whatsapp-for-linux
     zoom-us
