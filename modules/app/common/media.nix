88757e191db44608360d00ef8720543c4ed7eb22
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-11-30 19:57:11 +0100

    feat: Remove Podcasts; reorder OBS plugins

--- modules/app/common/media.nix
+++ modules/app/common/media.nix
@@ -4,9 +4,9 @@
     vlc
     (pkgs.wrapOBS {
       plugins = with pkgs.obs-studio-plugins; [
-        wlrobs
         obs-backgroundremoval
         obs-pipewire-audio-capture
+        wlrobs
       ];
     })
 
