a3ff55d46b96e13239c6b2426014b86dc7e6810c
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-12-05 20:26:26 +0100

    feat: Reconfigure webcam settings and enable v4l2loopback module

--- modules/hardware/common/peripherals/webcam.nix
+++ modules/hardware/common/peripherals/webcam.nix
@@ -1,12 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 let
   webcam-conf = pkgs.writeScriptBin "my-configure-webcam" ''
     # UGREEN 2K Webcam
-    v4l2-ctl -d /dev/video0 --set-ctrl=brightness=-30
-    v4l2-ctl -d /dev/video0 --set-ctrl=contrast=41
-    v4l2-ctl -d /dev/video0 --set-ctrl=saturation=80
-    v4l2-ctl -d /dev/video0 --set-ctrl white_balance_automatic=0
-    v4l2-ctl -d /dev/video0 --set-fmt-video=width=2560,height=1440,pixelformat=MJPG
+    v4l2-ctl --set-ctrl=brightness=-30
+    v4l2-ctl --set-ctrl=contrast=41
+    v4l2-ctl --set-ctrl=saturation=80
+    v4l2-ctl --set-ctrl white_balance_automatic=0
+    v4l2-ctl --set-fmt-video=width=2560,height=1440,pixelformat=MJPG
   '';
 in
 {
@@ -28,4 +28,12 @@ in
       StandardOutput = "journal";
     };
   };
+
+  boot.kernelModules = [ "v4l2loopback" ];
+  boot.extraModulePackages = [
+    config.boot.kernelPackages.v4l2loopback
+  ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
 }
