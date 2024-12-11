153bbef48e45f6d43a937dfa8cdfb16ab04e37ae
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-12-11 12:29:20 +0100

    fix: Update HDMI monitor configuration in HyprWM settings

--- modules/desktop/hyprwm/hyprVariables.nix
+++ modules/desktop/hyprwm/hyprVariables.nix
@@ -3,7 +3,7 @@
     Desktop = ''
       # ---- Desktop-specific Monitor Setup ---- #
       monitor = DP-1, 1920x1080@180, 0x0, 1
-      monitor = HDMI-A-2, 1920x1080, 1920x0, 1
+      monitor = HDMI-A-1, 1920x1080, 1920x0, 1
 
       workspace = 1,  monitor:DP-1, default:true
       workspace = 2,  monitor:DP-1
@@ -12,9 +12,9 @@
       workspace = 5,  monitor:DP-1
       workspace = 6,  monitor:DP-1
       workspace = 7,  monitor:DP-1
-      workspace = 8,  monitor:HDMI-A-2, default:true
-      workspace = 9,  monitor:HDMI-A-2
-      workspace = 10, monitor:HDMI-A-2
+      workspace = 8,  monitor:HDMI-A-1, default:true
+      workspace = 9,  monitor:HDMI-A-1
+      workspace = 10, monitor:HDMI-A-1
     '';
     Laptop = ''
       # ---- Laptop-specific Monitor Setup ---- #
