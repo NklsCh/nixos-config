b166a15cf0038edcf75defc42e6989ab1aee6f21
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-12-12 16:21:13 +0100

    feat: Add Proton Mail desktop entry for home-manager users

--- modules/app/common/communication.nix
+++ modules/app/common/communication.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, username, ... }:
 {
   environment.systemPackages = with pkgs; [
     element-desktop
@@ -9,4 +9,15 @@
     whatsapp-for-linux
     zoom-us
   ];
+  home-manager.users.${username} = {
+    home.file.".local/share/applications/proton-mail.desktop".text = ''
+      [Desktop Entry]
+      Name=Proton Mail
+      Exec=proton-mail --ozone-platform=x11
+      Terminal=false
+      Type=Application
+      Icon=${pkgs.protonmail-desktop}/share/pixmaps/proton-mail.png
+      Categories=Network;Email;
+    '';
+  };
 }
