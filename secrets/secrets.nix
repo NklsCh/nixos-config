482aafc9686d0dc55079f5709da920949f6f74f4
Niklas Choinowski <niklas.choinowski@gmail.com>
2025-01-14 19:33:28 +0100

    chore: Remove unused user from secrets configuration

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,5 @@
 let
   choinowski = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKRcnvF2EYThdhGp45gfbOck/ZXDZR5CrBiMm0eIx3Vl choinowski@Desktop";
-  users = [ choinowski ];
 in
 {
   "bw_session.age".publicKeys = [ choinowski ];
