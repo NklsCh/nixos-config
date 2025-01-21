cd293fdc6e060a4577392164037d471269378c1f
Niklas Choinowski <v3rm1n@tutamail.com>
2025-01-21 16:47:34 +0100

    feat: Add nix logo SVG and update configuration for logo display

--- assets/default.nix
+++ assets/default.nix
@@ -3,6 +3,7 @@
   home-manager.users.${username} = _: {
     home.file = {
       ".config/backgrounds".source = ./wallpapers;
+      "nixlogo.svg".source = ./logo/nix-snowflake.svg;
     };
   };
 }
