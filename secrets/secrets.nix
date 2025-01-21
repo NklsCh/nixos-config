f4386b76ec9add45120afb7cba990a2bca55a17c
Niklas Choinowski <v3rm1n@tutamail.com>
2025-01-21 21:16:38 +0100

    chore: Removed bitwarden secret

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,5 +2,5 @@ let
   choinowski = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKRcnvF2EYThdhGp45gfbOck/ZXDZR5CrBiMm0eIx3Vl choinowski@Desktop";
 in
 {
-  "bw_session.age".publicKeys = [ choinowski ];
+  # "filename".publicKeys = [ choinowski ];
 }
