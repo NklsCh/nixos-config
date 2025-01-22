6c7ed39d1f817472b1fdd090ce71205918459f5c
Niklas Choinowski <v3rm1n@tutamail.com>
2025-01-22 10:26:42 +0100

    feat: Update workspace configurations and remove Bitwarden session handling

--- modules/desktop/hyprwm/hyprVariables.nix
+++ modules/desktop/hyprwm/hyprVariables.nix
@@ -6,13 +6,13 @@
       monitor = HDMI-A-2, 1920x1080, 1920x0, 1
 
       workspace = 1,  monitor:DP-1, default:true
-      workspace = 2,  monitor:DP-1
+      workspace = 2,  monitor:HDMI-A-2, default:true
       workspace = 3,  monitor:DP-1
       workspace = 4,  monitor:DP-1
       workspace = 5,  monitor:DP-1
       workspace = 6,  monitor:DP-1
       workspace = 7,  monitor:DP-1
-      workspace = 8,  monitor:HDMI-A-2, default:true
+      workspace = 8,  monitor:DP-1
       workspace = 9,  monitor:HDMI-A-2
       workspace = 10, monitor:HDMI-A-2
     '';
@@ -24,11 +24,11 @@
   };
   WindowRules = {
     Desktop = ''
-      # General layout rule for workspace 8
-      workspace    = 8,split:v
-      windowrulev2 = workspace 8 silent, class:^(discord)$
+      # General layout rule for workspace 2
+      workspace    = 2,split:v
+      windowrulev2 = workspace 2 silent, class:^(discord)$
       windowrulev2 = tile, class:^(discord)$
-      windowrulev2 = workspace 8 silent, title:^(Spotify Premium)$
+      windowrulev2 = workspace 2 silent, title:^(Spotify Premium)$
       windowrulev2 = tile, title:^(Spotify Premium)$
 
       # General layout rule for workspace 7
--- modules/security/encryption/bitwarden.nix
+++ modules/security/encryption/bitwarden.nix
@@ -1,16 +1,10 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
-  age.secrets.bw_session = {
-    file = ../../../secrets/bw_session.age;
-  };
   environment = {
     systemPackages = with pkgs; [
     bitwarden
     bitwarden-cli
     bitwarden-menu
     ];
-    sessionVariables = {
-      BW_SESSION = "$(cat ${config.age.secrets.bw_session.path})";
-    };
   };
 }
