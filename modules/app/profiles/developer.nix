26f88f76366a3e2dd2bf59e22e284f0d9624b33a
Niklas Choinowski <v3rm1n@tutamail.com>
2025-02-17 08:40:14 +0100

    chore: Remove zed-editor and commented JetBrains IDEs from developer profile

--- modules/app/profiles/developer.nix
+++ modules/app/profiles/developer.nix
@@ -2,12 +2,7 @@
 {
   environment.systemPackages = with pkgs; [
     #--- Editors ---#
-    zed-editor
     vscode
-    #--- JetBrains ---#
-    # jetbrains.phpstorm
-    # jetbrains.webstorm
-    # jetbrains.idea-ultimate
     #--- Tools ---#
     gitkraken
     devenv
