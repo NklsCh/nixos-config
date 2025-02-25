140fcfd5958cecac16d24b823ffcde223fbe8a2e
Niklas Choinowski <v3rm1n@tutamail.com>
2025-02-25 16:35:44 +0100

    fix: Typo

--- hosts/Desktop/hardware-specific.nix
+++ hosts/Desktop/hardware-specific.nix
@@ -4,7 +4,7 @@
     "usb_storage"
   ];
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 
   fileSystems."/media/512-GB-SSD" = {
     device = "/dev/disk/by-uuid/ae015b22-1795-4bda-8f60-8a06cbf39d5a";
--- hosts/Laptop/hardware-specific.nix
+++ hosts/Laptop/hardware-specific.nix
@@ -1,3 +1,3 @@
 { pkgs, ... }: {
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 }
