66874fe641dac560a3ee749a80e5fd6797682707
Niklas Choinowski <v3rm1n@tutamail.com>
2025-03-20 22:08:54 +0100

    feat: enable Gnome Keyring for greetd service in PAM configuration

--- modules/security/authentication/pam.nix
+++ modules/security/authentication/pam.nix
@@ -1,6 +1,7 @@
 { ... }:
 {
   security.pam.services.login.enableGnomeKeyring = true;
+  security.pam.services.greetd.enableGnomeKeyring = true;
   security.pam.services.hyprlock = {
     text = ''
       auth      required  pam_gnome_keyring.so
