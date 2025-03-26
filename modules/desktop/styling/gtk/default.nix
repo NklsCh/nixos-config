97510bf61df37780b01b9ba29ea18ab022e3fd4c
Niklas Choinowski <v3rm1n@tutamail.com>
2025-03-26 09:20:00 +0100

    feat: enable dark theme preference for GTK applications

--- modules/desktop/styling/gtk/default.nix
+++ modules/desktop/styling/gtk/default.nix
@@ -23,6 +23,16 @@
         package = pkgs.bibata-cursors; # pkgs.adwaita-icon-theme;
         name = "Bibata-Modern-Ice"; # "Adwaita";
       };
+      gtk3.extraConfig = {
+        Settings = ''
+          gtk-application-prefer-dark-theme=1
+        '';
+      };
+      gtk4.extraConfig = {
+        Settings = ''
+          gtk-application-prefer-dark-theme=1
+        '';
+      };
     };
   };
 }
