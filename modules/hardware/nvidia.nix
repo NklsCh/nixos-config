1003b1227a460a05c2f61445d0b13ea4f660b4c1
Niklas Choinowski <v3rm1n@tutamail.com>
2025-04-03 14:53:53 +0200

    feat: update NVIDIA package from beta to stable version

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -25,7 +25,7 @@ in
       powerManagement.finegrained = false;
       open = false;
       nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.beta;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
   };
 }
