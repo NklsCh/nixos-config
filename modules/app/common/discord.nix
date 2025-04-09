5e7e6a0fd906cf5cfe24d2d6f666d079af6743d2
Niklas Choinowski <v3rm1n@tutamail.com>
2025-04-09 14:58:37 +0200

    feat: add user-specific Discord desktop entry configuration

--- modules/app/common/discord.nix
+++ modules/app/common/discord.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, username, ... }:
 {
   environment.systemPackages = with pkgs; [
     (discord.override {
@@ -6,4 +6,17 @@
       withVencord = true;
     })
   ];
+  home-manager.users.${username} = {
+    home.file = {
+      ".local/share/applications/Discord.desktop".text = ''
+        [Desktop Entry]
+        Name=Discord
+        Exec=discord --ozone-platform=x11
+        Terminal=false
+        Type=Application
+        Icon=${pkgs.discord}/share/pixmaps/discord.png
+        Categories=Network;Chat;
+      '';
+    };
+  };
 }
