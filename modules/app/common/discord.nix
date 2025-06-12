f0a5c155e41d73023ce56031156438a895e95fc4
Niklas Choinowski <niklas.choinowski@gmail.com>
2025-06-12 16:59:30 +0200

    style: comment out home-manager configuration for Discord

--- modules/app/common/discord.nix
+++ modules/app/common/discord.nix
@@ -6,17 +6,19 @@
       withVencord = true;
     })
   ];
-  home-manager.users.${username} = {
-    home.file = {
-      ".local/share/applications/Discord.desktop".text = ''
-        [Desktop Entry]
-        Name=Discord
-        Exec=discord --ozone-platform=x11
-        Terminal=false
-        Type=Application
-        Icon=${pkgs.discord}/share/pixmaps/discord.png
-        Categories=Network;Chat;
-      '';
+  /*
+    home-manager.users.${username} = {
+      home.file = {
+        ".local/share/applications/Discord.desktop".text = ''
+          [Desktop Entry]
+          Name=Discord
+          Exec=discord --ozone-platform=x11
+          Terminal=false
+          Type=Application
+          Icon=${pkgs.discord}/share/pixmaps/discord.png
+          Categories=Network;Chat;
+        '';
+      };
     };
-  };
+  */
 }
