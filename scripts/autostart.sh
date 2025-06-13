e030f02fcebec77f09ae014263ab1c143f5d400a
Niklas Choinowski <niklas.choinowski@gmail.com>
2025-06-13 07:54:08 +0200

    fix: update gnome-keyring-daemon to start with ssh component

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Start background services
-gnome-keyring-daemon --start --components=secrets &
+gnome-keyring-daemon --start --components=ssh,secrets &
 hyprpaper &
 nm-applet --indicator &
 ags &
