1be67e27935e944e6a4294e389b48493991b6590
Niklas Choinowski <niklas.choinowski@gmail.com>
2025-06-15 22:28:56 +0200

    refactor: simplify GnuPG configuration and remove unused user settings

--- modules/security/encryption/gpg/default.nix
+++ modules/security/encryption/gpg/default.nix
@@ -1,25 +1,11 @@
-{ username, pkgs, ... }:
+{ pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    gnupg
-    pinentry-curses
-    pinentry-gnome3
-  ];
-
-  home-manager.users.${username} = _: {
-    home.file = {
-      ".gnupg/gpg-agent.conf".source = ./gpg-agent.conf;
+  programs.gnupg = {
+    agent.enable = true;
+    agent.enableSSHSupport = true;
+    agent.pinentryPackage = pkgs.pinentry-gnome3;
+    agent.settings = {
+      allow-loopback-pinentry = true;
     };
   };
-
-  /*
-    programs.gpg = {
-      agent.enable = true;
-      agent.enableSSHSupport = true;
-      agent.pinentryPackage = pkgs.pinentry-gnome3;
-      agent.settings = {
-        allow-loopback-pinentry = true;
-      };
-    };
-  */
 }
