0ffd6cb689c7729ad988df30329cb49493ea9b3e
Niklas Choinowski <niklas.choinowski@gmail.com>
2025-06-16 17:20:57 +0200

    feat: enhance hyprpanel and age configurations with weather API integration

--- modules/security/encryption/age.nix
+++ modules/security/encryption/age.nix
@@ -1,6 +1,36 @@
-{ agenix, system, ... }:
+{
+  agenix,
+  system,
+  username,
+  ...
+}:
 {
   environment.systemPackages = [
     agenix.packages."${system}".default
   ];
+
+  age = {
+    identityPaths = [
+      "/home/${username}/.ssh/agenix_key"
+    ];
+    secrets = {
+      weatherAPI.file = ../../../secrets/weatherAPI.age;
+    };
+  };
+
+  home-manager.users.${username} = {
+    imports = [
+      agenix.homeManagerModules.default
+    ];
+
+    age = {
+      identityPaths = [
+        "/home/${username}/.ssh/agenix_key"
+      ];
+
+      secrets = {
+        weatherAPI.file = ../../../secrets/weatherAPI.age;
+      };
+    };
+  };
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,8 @@
 let
-  choinowski = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKRcnvF2EYThdhGp45gfbOck/ZXDZR5CrBiMm0eIx3Vl choinowski@Desktop";
+  userPublicKey = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLM5QhQNZGcHimWcCqS/I/l1KY1xBKAajPhY4xzd/v0 v3rm1n@Desktop"
+  ];
 in
 {
-  # "filename".publicKeys = [ choinowski ];
+  "weatherAPI.age".publicKeys = userPublicKey;
 }
