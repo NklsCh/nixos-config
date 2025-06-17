c5f5fff175b1f968187966e24b796230f8f24d48
Niklas Choinowski <niklas.choinowski@gmail.com>
2025-06-17 18:50:58 +0200

    refactor: reorganize home-manager user configuration for clarity

--- modules/desktop/xdg/default.nix
+++ modules/desktop/xdg/default.nix
@@ -35,25 +35,28 @@
   };
 
   home-manager.users.${username} = _: {
-    home.file = {
-      ".config/user-dirs.dirs".text = ''
-        XDG_DESKTOP_DIR="$HOME/Desktop"
-        XDG_DOWNLOAD_DIR="$HOME/Downloads"
-        XDG_TEMPLATES_DIR="$HOME/Templates"
-        XDG_PUBLICSHARE_DIR="$HOME/Public"
-        XDG_DOCUMENTS_DIR="$HOME/Documents"
-        XDG_MUSIC_DIR="$HOME/Music"
-        XDG_PICTURES_DIR="$HOME/Pictures"
-        XDG_VIDEOS_DIR="$HOME/Videos"
-      '';
+    targets.genericLinux.enable = true;
+    /*
+      home.file = {
+        ".config/user-dirs.dirs".text = ''
+          XDG_DESKTOP_DIR="$HOME/Desktop"
+          XDG_DOWNLOAD_DIR="$HOME/Downloads"
+          XDG_TEMPLATES_DIR="$HOME/Templates"
+          XDG_PUBLICSHARE_DIR="$HOME/Public"
+          XDG_DOCUMENTS_DIR="$HOME/Documents"
+          XDG_MUSIC_DIR="$HOME/Music"
+          XDG_PICTURES_DIR="$HOME/Pictures"
+          XDG_VIDEOS_DIR="$HOME/Videos"
+        '';
 
-      #--- Dirs to Create ---#
-      "Desktop/create".text = "";
-      "Downloads/create".text = "";
-      "Documents/create".text = "";
-      "Music/create".text = "";
-      "Pictures/create".text = "";
-      "Videos/create".text = "";
-    };
+        #--- Dirs to Create ---#
+        "Desktop/create".text = "";
+        "Downloads/create".text = "";
+        "Documents/create".text = "";
+        "Music/create".text = "";
+        "Pictures/create".text = "";
+        "Videos/create".text = "";
+      };
+    */
   };
 }
