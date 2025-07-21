70aa7e5eda09559e9f0a505dd786b59b8afefb0b
Niklas Choinowski <niklas.choinowski@gmail.com>
2025-07-21 10:22:16 +0200

    feat: enhance chromium configuration by adding options to disable various Brave features and set startup URLs

--- modules/applications/browsing/chromium.nix
+++ modules/applications/browsing/chromium.nix
@@ -7,10 +7,19 @@
   programs.chromium = {
     enable = true;
     extraOpts = {
+      "AudioSandboxEnabled" = false;
       "AutofillAddressEnabled" = false;
       "AutofillCreditCardEnabled" = false;
       "BlockThirdPartyCookies" = true;
+      "BraveAIChatEnabled" = 0; # Disable Brave AI Chat
+      "BraveNewsDisabled" = 1; # Disable Brave News
+      "BraveRewardsDisabled" = 1; # Disable Brave Rewards
+      "BraveTalkDisabled" = 1; # Disable Brave Talk
+      "BraveVPNDisabled" = 1; # Disable Brave VPN
+      "BraveWalletDisabled" = 1; # Disable Brave Wallet
       "DefaultSearchProviderEnabled" = true;
+      "RestoreOnStartup" = 4; # Restore specified pages
+      "RestoreOnStartupURLs" = [ "https://start.v3rm1n.dev" ];
       #"DefaultSearchProviderName" = "Startpage";
       #"DefaultSearchProviderSearchURL" = "https://www.startpage.com/do/search?query={searchTerms}";
       #"DefaultSearchProviderSuggestURL" = "https://www.startpage.com/do/search?query={searchTerms}";
