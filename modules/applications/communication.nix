d27731932ca57bf47188b7bb9428f67ab587fff3
Niklas Choinowski <niklas.choinowski@proton.me>
2025-07-25 11:34:45 +0200

    feat: add Proton Mail desktop entry and include it in system packages

--- modules/applications/communication.nix
+++ modules/applications/communication.nix
@@ -1,10 +1,25 @@
-{ pkgs, ... }:
+{ pkgs, username, ... }:
 {
   environment.systemPackages = with pkgs; [
     element-desktop
+    protonmail-desktop
     signal-desktop
     teamspeak6-client
     whatsapp-for-linux
     zoom-us
   ];
+
+  home-manager.users.${username} = {
+    home.file = {
+      ".local/share/applications/proton-mail.desktop".text = ''
+        [Desktop Entry]
+        Name=Proton Mail
+        Exec=proton-mail --ozone-platform=x11
+        Terminal=false
+        Type=Application
+        Icon=${pkgs.protonmail-desktop}/share/pixmaps/proton-mail.png
+        Categories=Network;Email;
+      '';
+    };
+  };
 }
